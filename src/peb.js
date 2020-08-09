25ab97a65fc9de91bc65a22020e16c04b1c7d98e
TheColdPot <66854652+TheColdPot@users.noreply.github.com>
2020-08-09 10:47:29 +0800

    Main: Update source code

--- src/peb.js
+++ src/peb.js
@@ -4,87 +4,150 @@
  */
 ;(function() {
 	console.log("Peb.js is available!");
-	
-	String.prototype.multi = function(t) {
-		return Array(t + 1).join(this);
+	let sth = document.createElement("link");
+	sth.rel = "stylesheet";
+	sth.href = "./css/peb-basic.css";
+	document.head.appendChild(sth);
+	String.prototype.multi = function(t, c="") {
+		return Array(t).fill(this).join(c);
 	};
 	Object.prototype.forEach = function(f) {
-		for (var i in this) {
+		for (let i in this) {
 			f(this[i], i, this);
 		}
-	}
-	let su = "undefined"
-	window.gen = function(cd, ...args) {
-		return ({
-			el: function(n, c="", a={"pb-empty": "true"}) {
-				let rn = document.createElement(n);
-				rn.appendChild(document.createTextNode(c));
-				for (var i in a) {
-					rn.setAttribute(i, a[i]);
-				};
-				return rn;
-			},
-			tn: function(t) {
-				return document.createTextNode(t)
-			}
-		})[cd](...args);
 	};
+	customElements.define("p-trans", class PebTransElement extends HTMLElement {
+		constructor() {
+			super();
+		}
+	});
+	customElements.define("p-mark", class PebMarkElement extends HTMLElement {
+		constructor() {
+			super();
+			this.style.color = "attr(color), inherit";
+			this.style.fontFamily = "attr(font), inherit"
+		}
+	})
+	const exist = function(v) {
+		if (v === void 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	window.translation = function(table) {
+		document.querySelectorAll("peb-trans").forEach((c) => {
+			c.innerHTML = table[c.getAttribute("p-lang")][c.getAttribute("p-word")];
+		});
+	};
+	window.sum = function(...values) {
+		if (values[0].__proto__ === Array.prototype) {
+			return sum(...(values[0]))
+		} else {
+			return eval(values.join("+"))
+		}
+	};
+	window.gen = (function() {
+		this.ele = function(n, c="", a={}) {
+			let r = document.createElement(n)
+			r.appendChild(document.createTextNode(String(c)));
+			for (let i of Object.keys(a)) {
+				r.setAttribute(i, a[i]);
+			}
+			return r;
+		},
+		this.tnode = function(t) {
+			return document.createTextNode(String(t));
+		}
+	})();
 	window.pb = function(e, s) {
-		if (e[0] === "#" || s + 1) {
+		if (e[0] === "#" || exist(s)) {
 			let ix = s || 0
 			  , el = document.querySelectorAll(e).item(ix);
-			return new (function ResultElement() {
+			return new (function RElement() {
 				this.selector = e,
 				this.id = el.id,
 				this.cls = el.classList,
 				this.tag = el.tagName;
 				this.__proto__ = {
 					attr: function(n, f) {
-						let ft = String(f);
-						if (ft === su) {
-							return el.getAttribute(n);
+						if (!exist(n)) {
+							return el.attributes
+						} else if (!exist(f)) {
+							switch (typeof(n)) {
+								case "string":
+									return el.getAttribute(n);
+								case "object":
+									for (let i in n) {
+										el.setAttribute(i, n[i]);
+									}
+									break;
+							}
 						} else {
-							return el.setAttribute(n, ft);
+							return el.setAttribute(n, String(f));
+						}
+					},
+					dats: function(n, f) {
+						if (!exist(n)) {
+							return el.dataset;
+						} else if (!exist(f)) {
+							switch (typeof(n)) {
+								case "string":
+									return el.dataset[n];
+								case "object":
+									for (let i in n) {
+										el.dataset[i] = n[i];
+									}
+							}
+						} else {
+							return el.dataset[n] = String(f);
 						}
 					},
 					item: function(n, f) {
 						if (f) {
-							return el[n]
+							return el[n];
 						} else {
-							return el[n] = f
+							return el[n] = f;
 						}
 					},
 					css: function(n, f) {
-						let ft = String(f);
-						if (ft === su) {
-							return el.style[n]
+						if (!exist(f)) {
+							switch (typeof(n)) {
+								case "string":
+									return el.style[n];
+								case "object":
+									for (let i in n) {
+										el.dataset[i] = n[i];
+									}
+							}
 						} else {
+							let ft = String(f)
 							return el.style[n] = ft
 						}
 					},
-					insert: function(v) {	
-						return el.appendChild(v);
+					insert: function(...v) {
+						for (let i in v) {
+							el.appendChild(v[i]);
+						}
 					},
 					del: function() {
 						return el.parentNode.removeChild(el);
 					},
 					htm: function(f) {
-						let ft = String(f);
-						if (ft === su) {
-							return el.innerHTML
+						if (exist(f)) {
+							return el.innerHTML = String(f);
 						} else {
-							return el.innerHTML = ft
+							return el.innerHTML;
 						}
 					},
 					txt: function() {
 						return el.innerText
 					},
 					val: function(f) {
-						let ft = String(f);
-						if (ft === su) {
-							return el.value
+						if (exist(f)) {
+							return el.value = String(f);
 						} else {
-							return el.value = ft
+							return el.value;
 						}
 					},
 					hide: function() {
@@ -92,26 +155,44 @@
 						return el.style.display = "none";
 					},
 					show: function(t) {
-						if (t) {
+						if (exist(t)) {
 							return el.style.display = String(t);
 						} else {
-							return el.style.display = el.dbh
+							return el.style.display = el.dbh;
+						}
+					},
+					evt: function(n, f) {
+						if (exist(f)) {
+							el.addEventListener(n, f);
+						} else {
+							switch (typeof(n)) {
+								case "string":
+									el.removeEventListener(n)
+									break;
+								case "object":
+									for (let i in n) {
+										el.addEventListener(i, n[i])
+									}
+							}
 						}
 					}
 				}
-			})();
+			})(); 
 		} else {
-			return new (function ResultElements(){
+			return new (function RElementsCollection() {
 				let es = document.querySelectorAll(e)
+				es.forEach((c, ix) => {
+					this[ix] = c;
+				});
 				this.length = es.length;
 				this.__proto__ = {
-					each: function(f) {
-						for (var i in es) {
-							f(pb(e, i), i);
-						}
+					forEach: function(f) {
+						es.forEach((_c, ix) => {
+							f(pb(e, ix), ix);
+						})
 					},
 					item: function(i) {
-						return pb(e, i)
+						return pb(e, i);
 					}
 				}
 			})();
