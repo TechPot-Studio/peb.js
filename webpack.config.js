ff26b7d97f7136c170a9ccd5ee82005cf45ef0d0
TheColdPot <guohw979@outlook.com>
2020-11-07 16:01:48 +0800

    Build: Update script

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
                         plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 }
+            },
+            {
+                test: /\.css$/g,
+                use: [ 'style-loader', 'css-loader' ]
             }
         ]
     },
